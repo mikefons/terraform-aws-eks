3170585bd5b5d8542131c7404aba1382b2a3caa8
mikefons <fonseca.michael@gmail.com>
2021-12-24 13:57:36 -0500

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -13,7 +13,7 @@ terraform {
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = ">= 1.11.1"
+      version = ">= 2.7.1"
     }
     cloudinit = {
       source  = "hashicorp/cloudinit"
